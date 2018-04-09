00ee6c7a1c8b1fc2a6aecfe97be30fdac77ecd28
yaphet <zhaoxiaohong@afcatrbx.com.cn>
2018-04-09 16:18:43 +0800

    header file change

--- LJCore/LJCore/LJCore.h
+++ LJCore/LJCore/LJCore.h
@@ -6,22 +6,22 @@
 //  Copyright © 2018年 Yaphets. All rights reserved.
 //
 
-#import <LJCore/LJCommonLogMessage.h>
-#import <LJCore/LJBase64Encode.h>
-#import <LJCore/LJCodeDefine.h>
-#import <LJCore/LJJavascriptBridgeBaseProtocol.h>
-#import <LJCore/LJWebViewConfigurationProtocol.h>
-#import <LJCore/LJWebViewJavascriptBridge.h>
-#import <LJCore/LJWKWebViewJavascriptBridge.h>
-#import <LJCore/LJJavascriptBridge.h>
-#import <LJCore/LJMessageBaseObject.h>
-#import <LJCore/UIWebView+LJCategory.h>
+#import "LJCommonLogMessage.h"
+#import "LJBase64Encode.h"
+#import "LJCodeDefine.h"
+#import "LJJavascriptBridgeBaseProtocol.h"
+#import "LJWebViewConfigurationProtocol.h"
+#import "LJWebViewJavascriptBridge.h"
+#import "LJWKWebViewJavascriptBridge.h"
+#import "LJJavascriptBridge.h"
+#import "LJMessageBaseObject.h"
+#import "UIWebView+LJCategory.h"
 //! Project version number for LJCore.
 FOUNDATION_EXPORT double LJCoreVersionNumber;
 
 //! Project version string for LJCore.
 FOUNDATION_EXPORT const unsigned char LJCoreVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <LJCore/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import "PublicHeader.h"
 
 
